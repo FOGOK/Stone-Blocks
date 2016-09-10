a131c86d85d4319b2fc3b13ac628b238146de471
1FOGOK1 <fogok.production@gmail.com>
2016-09-10 22:52:10 +0300

    Initial commit

---
+++ android/src/com/java4game/cuadro/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.java4game.cuadro;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.java4game.cuadro.Gm;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Gm(), config);
+	}
+}
---
+++ desktop/src/com/java4game/cuadro/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.java4game.cuadro.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.java4game.cuadro.Gm;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Gm(), config);
+	}
+}
